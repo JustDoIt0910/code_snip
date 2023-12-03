f9e291565c457d26b90af065a8cbc5a2a104267f
JustDoIt0910 <1396812324@qq.com>
2023-12-03 13:17:47 +0800

    update(threadsafe_queue.h)

--- CCIA/threadsafe_queue.cc
+++ CCIA/threadsafe_queue.cc
@@ -2,7 +2,21 @@
 // Created by just do it on 2023/12/3.
 //
 #include "threadsafe_queue.h"
+#include <thread>
+#include <unistd.h>
+#include <iostream>
 
 int main() {
     threadsafe_queue<int> queue;
+
+    std::thread t([&queue]() {
+        sleep(1);
+        queue.push(10);
+    });
+
+    int val;
+    queue.wait_and_pop(val);
+    std::cout << val << std::endl;
+
+    t.join();
 }
